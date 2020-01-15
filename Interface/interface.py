c02bbeb4baa1c67f562487b6ce7c55ac1628b42c
Luci <48629865+LuciFez@users.noreply.github.com>
2020-01-15 13:33:42 +0200

    Add files via upload

--- Interface/interface.py
+++ Interface/interface.py
@@ -1,31 +1,31 @@
 from tkinter import *
 from tkinter import ttk
-
-master = Tk()
-master.geometry("300x100+10+20")
-Label(master, text='Add a description').grid(row=0)
-e1 = Entry(master)
-e1.grid(row=0, column=1)
+from functools import partial
 
 
-def user_input():
-    query = e1.get()
-    return query
+def draw():
+    master = Tk()
+    master.geometry("605x255")
+    master.resizable(False,False)
 
+    searchBar = Entry(master,width = 80)
+    searchBar.grid(row=0, column=0, columnspan = 2)
 
-def get_result():
-    window = Tk()
     cols = ('Title', 'Genre', 'Description')
-    Label(window, text="Results").grid(row=0)
-    listBox = ttk.Treeview(window, columns=cols, show='headings')
+    listBox = ttk.Treeview(master, columns=cols, show='headings')
     for col in cols:
         listBox.heading(col, text=col)
-    listBox.grid(row=1, column=0, columnspan=2)
+    listBox.grid(row=1,column=0,columnspan=3)
+    
+    searchButton = Button(master, text="Search", command= partial(user_input,searchBar,listBox),relief=GROOVE,width=16)
+    searchButton.grid(row=0, column=2)
 
+    
+    mainloop()
 
-b1 = Button(master, text="Search", command=user_input)
-b2 = Button(master, text="View results", command=get_result)
+def user_input(searchBar,listBox):
+    searchBarText = searchBar.get()
+    listBox.insert("","end", values=("a","b","c")) #aici pui din ce returneaza AI ul , a-primul camp, b-al doilea etc.
+    return searchBarText
 
-b1.grid(row=0, column=2)
-b2.grid(row=1, column=1)
-mainloop()
+draw()
