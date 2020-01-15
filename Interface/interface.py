60cee224a872b32cc6be36828d7e1fa5a246e517
Luci <48629865+LuciFez@users.noreply.github.com>
2020-01-15 14:00:45 +0200

    Add files via upload

--- Interface/interface.py
+++ Interface/interface.py
@@ -8,8 +8,14 @@ def draw():
     master.geometry("605x255")
     master.resizable(False,False)
 
-    searchBar = Entry(master,width = 80)
-    searchBar.grid(row=0, column=0, columnspan = 2)
+    drop = ttk.Combobox(master,values=["1","2","3","4","5","6","7","8","9","10"])
+    drop.set("Nr Results")
+    drop.grid(column=0,row=0)
+
+
+
+    searchBar = Entry(master,width = 55)
+    searchBar.grid(row=0, column=1)
 
     cols = ('Title', 'Genre', 'Description')
     listBox = ttk.Treeview(master, columns=cols, show='headings')
@@ -17,13 +23,17 @@ def draw():
         listBox.heading(col, text=col)
     listBox.grid(row=1,column=0,columnspan=3)
     
-    searchButton = Button(master, text="Search", command= partial(user_input,searchBar,listBox),relief=GROOVE,width=16)
+    searchButton = Button(master, text="Search", command= partial(user_input,searchBar,listBox,drop),relief=GROOVE,width=16)
     searchButton.grid(row=0, column=2)
 
     
     mainloop()
 
-def user_input(searchBar,listBox):
+def user_input(searchBar,listBox,drop):
+    nr_results = 10
+    if(drop.get()!="Nr Results"):
+        nr_results = drop.get()
+    print(nr_results)
     searchBarText = searchBar.get()
     listBox.insert("","end", values=("a","b","c")) #aici pui din ce returneaza AI ul , a-primul camp, b-al doilea etc.
     return searchBarText
